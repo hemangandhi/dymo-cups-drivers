388e07858471310e1a37789cf029d7d8dc064068
hemangandhi <hemangandhi@gmail.com>
2018-10-04 20:53:26 -0400

    name

--- qr_script.py
+++ qr_script.py
@@ -7,10 +7,16 @@ import PIL
 import base64
 import sys
 import subprocess
+import requests as req
+from flask import Flask, render_template, request, redirect, jsonify
+
+app = Flask(__name__)
 
 SAMPLE_CODE_BASE_PATH = '/home/architect/dymo-cups-drivers/samples/test_label/'
 PHOTO_PATH = SAMPLE_CODE_BASE_PATH + 'tel.png'
 PRINTER_CMD = SAMPLE_CODE_BASE_PATH + 'TestLabel'
+
+LCS_URL = ''
 
 def email2qr(email):
     #construct the QR code
@@ -29,8 +35,30 @@ def run_printer(p_exec, email, qr_path):
     with open(qr_path, 'wb') as qr_out:
         img.save(qr_out, format='PNG')
     pr = subprocess.run(p_exec)
-    print(pr)
     return pr.returncode
+
+@app.route('/print', methods=['POST'])
+def web_req():
+    data = request.json()
+    if 'email' not in data:
+        return 400, 'Need email and name to print.'
+
+    if 'name' not in data:
+        r = requests.post(LCS_URL, json={'query': {'email': data['email']}})
+        rj = r.json()
+        try:
+            name = rj['body'][0]['first_name'] + rj['body'][0]['last_name']
+        except:
+            print(rj)
+            return 400, 'User not found or something... check cupcake'
+    else:
+        name = data['name']
+
+    role = data.get('role', 'Hacker')
+    print_proc = run_printer([PRINTER_CMD, 'labels', name, email, role], email, PHOTO_PATH)
+    if print_proc != 0:
+        return 500, 'Check cupcake... there might be trouble'
+    return 200, 'Success'
 
 if __name__ == '__main__':
     email = sys.argv[1] if len(sys.argv) >= 2 else 'rnd@hackru.org'
